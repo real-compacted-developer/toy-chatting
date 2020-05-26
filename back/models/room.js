042e4963196a048c41fe632a751cbf1337c71f12
kkoon9 <rndrnjs2003@naver.com>
2020-05-26 11:42:46 +0900

    Feat: room class 제작
    room은 중복이 안되므로 roomNumber를 key로, nicks를 value로 가지는 Map
    roomNumber : Numeric
    nicks : nickname이 중복이 안되므로 set으로 구성한다.
    create : roomNumber를 인자로 가지는 room을 생성하는 메서드
    delete : roomNumber를 인자로 가지는 room을 삭제하는 메서드
    join : roomNumber와 nickname을 인자로 가지는 room을 참가하는 메서드
    exit : roomNumber와 nickname을 인자로 가지는 room을 나가는 메서드
    hasRoom : roomNumber를 인자로 가지는 roomNumber를 번호로 가지는 방이 있는지 체크하는 메서드

---
+++ back/models/room.js
@@ -0,0 +1,44 @@
+// roomNumber:0000,
+// nicks: new Set()
+
+module.exports =  class Room{
+    constructor(){
+        this.rooms = new Map();
+    }
+
+    create(roomNumber){
+        if(this.rooms.has(roomNumber)){
+            return;
+        }
+
+        this.rooms.set(roomNumber, new Set());
+    }
+
+    delete(roomNumber){
+        if(!this.rooms.has(roomNumber)){
+            return;
+        }
+
+        this.rooms.delete(roomNumber);
+    }
+
+    join(roomNumber, nickname){
+        if(!this.rooms.has(roomNumber)){
+            return;
+        }
+
+        this.rooms.get(roomNumber).set(nickname);
+    }
+
+    exit(roomNumber, nickname){
+        if(!this.rooms.has(roomNumber)){
+            return;
+        }
+
+        this.rooms.get(roomNumber).delete(nickname);
+    }
+
+    hasRoom(roomNumber){
+        return this.rooms.has(roomNumber);
+    }
+}
