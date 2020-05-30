cf478f342929a720449be652d05a1d38ef81ecf3
kkoon9 <rndrnjs2003@naver.com>
2020-05-30 15:38:40 +0900

    style: tab Size 4 -> 2 로 변경
    ? Team Convention 맞추기 위해 Tab Size를 4에서 2로 변경했습니다.

--- back/models/room.spec.js
+++ back/models/room.spec.js
@@ -1,17 +1,16 @@
 // const jest = require('jest');
 const Room = require('./room');
 
-
 // test('adds 1 + 2 to equal 3', () => {
 //     expect(3).toBe(3);
 //   });
 
 test('roomTest', () => {
-    let myRoom = new Room();
+  let myRoom = new Room();
 
-    expect(myRoom.hasRoom(1111)).toBe(false);
+  expect(myRoom.hasRoom(1111)).toBe(false);
 
-    myRoom.create(1111);
+  myRoom.create(1111);
 
-    expect(myRoom.hasRoom(1111)).toBe(true);
+  expect(myRoom.hasRoom(1111)).toBe(true);
 });
