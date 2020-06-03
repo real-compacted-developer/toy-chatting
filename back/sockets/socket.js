2de51bd757bca9621bfe209baa550fd303ca2171
SkyLightQP <combbm@naver.com>
2020-06-03 18:20:24 +0900

    Feat: 방 입장, 퇴장 소켓 구현
    Feat: 가독성을 위해 소켓 코드 파일 추가

--- back/sockets/socket.js
+++ back/sockets/socket.js
@@ -1,10 +1,23 @@
 module.exports = () => {
   const io = require('../bin/www');
+  const OPCODE = require('./op-code');
 
   io.on('connect', (socket) => {
-    socket.on('chat', (nickname, msg) => {
-      io.emit('chat', nickname, msg);
-      console.log(`[CHAT] ${nickname}: ${msg}`);
+    socket.on(OPCODE.CHAT, (roomId, nickname, msg) => {
+      io.to(roomId).emit(OPCODE.CHAT, nickname, msg);
+      console.log(`[CHAT] #${roomId} ${nickname}: ${msg}`);
+    });
+
+    socket.on(OPCODE.JOIN, (id) => {
+      socket.join(id, () => {
+        io.to(id).emit(OPCODE.JOIN, `${id} 방에 누군가 들어왔습니다.`);
+      });
+    });
+
+    socket.on(OPCODE.LEAVE, (id) => {
+      socket.leave(id, () => {
+        io.to(id).emit(OPCODE.LEAVE, `${id} 방에서 누군가 나갔습니다.`);
+      });
     });
   });
 };
